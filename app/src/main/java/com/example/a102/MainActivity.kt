365219cd169a3f1ce2a128b35abff570562c555c
mahsa <mmahsamizabi@gmail.com>
2023-02-27 13:36:24 -0800

    saaaaa

--- app/src/main/java/com/example/a102/MainActivity.kt
+++ app/src/main/java/com/example/a102/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.a102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.accessibility.AccessibilityManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello from android!",Toast.LENGTH_LONG).show()
     }
 }
